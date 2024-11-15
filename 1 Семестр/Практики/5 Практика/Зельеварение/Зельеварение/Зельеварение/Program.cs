5698fe33714b75f0675153793137303de9802d9f
Belogrivtsev <nuclearthrone55@gmail.com>
2024-11-15 13:16:04 +0600

    обновил репозиторий (что-то доделано, что-то нет)

---
+++ 1 Семестр/Практики/5 Практика/Зельеварение/Зельеварение/Зельеварение/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+class App
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите количество действий");
+        var n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ввод производится в формате: (MIX или WATER или DUST или FIRE)пробел(ИНГРИДИЕНТ)");
+        var result = "";
+        for (int i = 0; i < n; i++)
+        {
+            var x = Convert.ToString(Console.ReadLine());
+            if (x.Substring(0,3) == "MIX")
+            {
+                //обработка ингридиента
+                var ingridient = x.Substring(4);
+                ingridient = ingridient.Replace(" ", "");
+                ingridient = ingridient.Replace("1", "");
+                ingridient = ingridient.Replace("2", "");
+                ingridient = ingridient.Replace("3", "");
+                ingridient = ingridient.Replace("4", "");
+                ingridient = ingridient.Replace("5", "");
+                ingridient = ingridient.Replace("6", "");
+                ingridient = ingridient.Replace("7", "");
+                ingridient = ingridient.Replace("8", "");
+                ingridient = ingridient.Replace("9", "");
+                ingridient = ingridient.Replace("0", "");
+                ingridient = ingridient.Replace("-", "");
+                //запись строки в результат
+                result = result + ingridient;
+                result = "MX" + result + "XM";
+            }
+            else if (x.Substring(0, 5) == "WATER")
+            {
+                var ingridient = x.Substring(6);
+                ingridient = ingridient.Replace(" ", "");
+                ingridient = ingridient.Replace("1", "");
+                ingridient = ingridient.Replace("2", "");
+                ingridient = ingridient.Replace("3", "");
+                ingridient = ingridient.Replace("4", "");
+                ingridient = ingridient.Replace("5", "");
+                ingridient = ingridient.Replace("6", "");
+                ingridient = ingridient.Replace("7", "");
+                ingridient = ingridient.Replace("8", "");
+                ingridient = ingridient.Replace("9", "");
+                ingridient = ingridient.Replace("0", "");
+                ingridient = ingridient.Replace("-", "");
+                //запись строки в результат
+                result = result + ingridient;
+                result = "WT" + result + "TW";
+            }
+            else if (x.Substring(0, 4) == "DUST")
+            {
+                var ingridient = x.Substring(5);
+                ingridient = ingridient.Replace(" ", "");
+                ingridient = ingridient.Replace("1", "");
+                ingridient = ingridient.Replace("2", "");
+                ingridient = ingridient.Replace("3", "");
+                ingridient = ingridient.Replace("4", "");
+                ingridient = ingridient.Replace("5", "");
+                ingridient = ingridient.Replace("6", "");
+                ingridient = ingridient.Replace("7", "");
+                ingridient = ingridient.Replace("8", "");
+                ingridient = ingridient.Replace("9", "");
+                ingridient = ingridient.Replace("0", "");
+                ingridient = ingridient.Replace("-", "");
+                //запись строки в результат
+                result = result + ingridient;
+                result = "DT" + result + "TD";
+            }
+            else if (x.Substring(0, 4) == "FIRE")
+            {
+                var ingridient = x.Substring(5);
+                ingridient = ingridient.Replace(" ", "");
+                ingridient = ingridient.Replace("1", "");
+                ingridient = ingridient.Replace("2", "");
+                ingridient = ingridient.Replace("3", "");
+                ingridient = ingridient.Replace("4", "");
+                ingridient = ingridient.Replace("5", "");
+                ingridient = ingridient.Replace("6", "");
+                ingridient = ingridient.Replace("7", "");
+                ingridient = ingridient.Replace("8", "");
+                ingridient = ingridient.Replace("9", "");
+                ingridient = ingridient.Replace("0", "");
+                ingridient = ingridient.Replace("-", "");
+                //запись строки в результат
+                result = result + ingridient;
+                result = "FR" + result + "RF";
+            }
+            else
+            {
+                Console.WriteLine("Ввод сделан не по формату, строка не засчитывается");
+            }
+        }
+        Console.WriteLine($"Получили заклинание {result}");
+    }
+}
---
+++ 1 Семестр/Практики/5 Практика/Компания ХХХ/Компания ХХХ/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class App
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите идентификационный номер (ХХХХ) и имя подчиненного (разделите их пробелом)");
+        Console.WriteLine("Программа закончит своё действие после ввода слова END");
+        for (int i = 0; i >= 0; i++)
+        {
+            var StrStart = Convert.ToString(Console.ReadLine());
+            if (StrStart == "END")
+            {
+                break;
+            }
+            else if (StrStart[4] != ' ')
+            {
+                Console.WriteLine("Error");
+            }
+            else
+            {
+
+            }
+        } 
+    }
+}
