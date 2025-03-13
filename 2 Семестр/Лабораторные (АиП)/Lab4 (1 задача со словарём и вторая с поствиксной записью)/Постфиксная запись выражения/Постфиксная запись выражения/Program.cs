60581d35b39d935ecac56c829f2916bfc899b693
Belogrivtsev <nuclearthrone55@gmail.com>
2025-03-13 23:49:18 +0600

    update: добавлена лаба по постфиксной записи выражения

---
+++ 2 Семестр/Лабораторные (АиП)/Lab4 (1 задача со словарём и вторая с поствиксной записью)/Постфиксная запись выражения/Постфиксная запись выражения/Program.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Reflection.Metadata.Ecma335;
+class App
+{
+
+    // арифметические операции :
+
+    public static int Sum(List<int> list, int previousResult, int coef)
+    {
+        if (coef == 1)
+        {
+            int result = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                result += list[i];
+            }
+            return result;
+        }
+        else
+        {
+            int result = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                result += list[i];
+            }
+            return previousResult + result;
+        }
+    }
+    public static int Diff(List<int> list, int previousResult, int coef)
+    {
+        if (coef == 1)
+        {
+            int result = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                result -= list[i];
+            }
+            return result;
+        }
+        else
+        {
+            int result = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                result -= list[i];
+            }
+            return previousResult - result;
+        }
+    }
+    public static int Multi(List<int> list, int previousResult, int coef)
+    {
+        if (coef == 1)
+        {
+            int result = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                result *= list[i];
+            }
+            return result;
+        }
+        else
+        {
+            int result = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                result *= list[i];
+            }
+            return previousResult * result;
+        }
+    }
+    public static int Split(List<int> list, int previousResult, int coef)
+    {
+        if (coef == 1)
+        {
+            int result = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i] == 0)
+                {
+                    Console.WriteLine("На ноль делить нельзя!");
+                    return -1;
+                }
+                else
+                {
+                    result /= list[i];
+                }
+            }
+            return result;
+        }
+        else
+        {
+            int result = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i] == 0)
+                {
+                    Console.WriteLine("На ноль делить нельзя!");
+                    return -1;
+                }
+                else
+                {
+                    result /= list[i];
+                }
+            }
+            if (result == 0)
+            {
+                Console.WriteLine("На ноль делить нельзя!");
+                return -1;
+            }
+            else
+            {
+                return previousResult / result;
+            }
+        }
+    }
+
+    // главный блок кода:
+
+    public static int PostFixExpression(string str)
+    {
+        int firstEntering = 1; // первое или не первое вхождение в код
+        int result = 0;
+        string operand = "";
+        int operationFlag = 0; // нужно для того, чтобы знать, что была использована операция
+        List<int> ListOfOperands = new List<int>();
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (firstEntering == 1)
+            {
+                if (str[i] != ' ' && !IsOperation(str[i]))
+                {
+                    operand += str[i];
+                }
+                else if (IsOperation(str[i]))
+                {
+                    result = DoOperation(str[i], ListOfOperands, result, 1);
+                    ListOfOperands.Clear();
+                    operationFlag = 1;
+                }
+                else if (operationFlag == 1)
+                {
+                    operand = "";
+                    operationFlag = 0;
+                }
+                else
+                {
+                    ListOfOperands.Add(Convert.ToInt32(operand));
+                    operand = "";
+                }
+                firstEntering = 0;
+            }
+            else
+            {
+                if (str[i] != ' ' && !IsOperation(str[i]))
+                {
+                    operand += str[i];
+                }
+                else if (IsOperation(str[i]))
+                {
+                    result = DoOperation(str[i], ListOfOperands, result, 0);
+                    ListOfOperands.Clear();
+                    operationFlag = 1;
+                }
+                else if (operationFlag == 1)
+                {
+                    operand = "";
+                    operationFlag = 0;
+                }
+                else
+                {
+                    ListOfOperands.Add(Convert.ToInt32(operand));
+                    operand = "";
+                }
+            }    
+        }
+        Console.WriteLine(result);
+        return 0;
+    }
+    public static int DoOperation(char operation, List<int> list, int previousResult, int coef)
+    {
+        List<char> OperationsList = new List<char>() {'+', '-', '*', '/'};
+        foreach (char i in OperationsList)
+        {
+            switch(operation)
+            {
+                case '+':
+                    return Sum(list, previousResult, coef);
+                case '-':
+                    return Diff(list, previousResult, coef);
+                case '*':
+                    return Multi(list, previousResult, coef);
+                case '/':
+                    return Split(list, previousResult, coef);
+            }
+        }
+        return 0;
+    }
+    public static bool IsOperation(char operation)
+    {
+        List<char> OperationsList = new List<char>() { '+', '-', '*', '/' };
+        foreach (char i in OperationsList)
+        {
+            if (operation == i)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Введите постфиксное выражение (пример: 2 2 + (=> 2 + 2)");
+        Console.WriteLine("Вводите выражение правильно, если программа обнаружит присутствие n операций подряд, то будет ошибка (например: 2 2 - +)");
+        string exp = Console.ReadLine();
+        Console.WriteLine("\nРезультат:\n");
+        PostFixExpression(exp);
+        
+    }
+}
